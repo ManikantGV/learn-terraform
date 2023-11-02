9fe067db2492e4b701cbeaa7cc1987aa9b06dc1a
Manikanta <manikanta.guntika@gmail.com>
2023-11-02 23:35:07 +0530

    Terraform

--- 02-datasource/main.tf
+++ 02-datasource/main.tf
@@ -17,13 +17,17 @@
 
 data "aws_security_groups" "test" {}
 
-#data "aws_security_group" "single" {
-#  count = length(data.aws_security_groups.test.ids)
-#  id=data.aws_security_groups.test.ids[count.index]
-#}
+data "aws_security_group" "single" {
+count = length(data.aws_security_groups.test.ids)
+id=data.aws_security_groups.test.ids[count.index]
+}
 
 output "all" {
   value = data.aws_security_groups.test
+}
+
+output "single" {
+  value = data.aws_security_group.single
 }
 
 
