b91a1d104cfb2b768f2b679d583aaa4b28596bbf
Manikanta <manikanta.guntika@gmail.com>
2023-11-02 23:52:20 +0530

    Terraform

--- loop/count/main.tf
+++ loop/count/main.tf
@@ -2,13 +2,13 @@ resource "null_resource" "null" {
   count = 10
 }
 
-#resource "null_resource" "fruits" {
-#  count = length(var.fruits)
-#  provisioner "local-exec" {
-#    command="echo fruit name - ${var.fruits[count.index]}"
-#  }
-#}
-#
-#variable "fruits" {
-#  default = ["apple", "bannana","orange"]
-#}
+resource "null_resource" "fruits" {
+  count = length(var.fruits)
+  provisioner "local-exec" {
+    command="echo fruit name - ${var.fruits[count.index]}"
+  }
+}
+
+variable "fruits" {
+  default = ["apple", "bannana","orange"]
+}
