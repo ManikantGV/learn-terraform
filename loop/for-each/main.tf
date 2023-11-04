261a1416e7153c1af71e4ad367d68b51398174bd
Manikanta <manikanta.guntika@gmail.com>
2023-11-04 23:45:00 +0530

    loop

--- loop/for-each/main.tf
+++ loop/for-each/main.tf
@@ -1,7 +1,10 @@
 resource "null_resource" "fruits" {
   #count=length(var.fruits)
+  for_each = var.fruits
   provisioner "local-exec" {
-    command="echo fruit name -${length(var.fruits)}"
+    #command="echo fruit name -${length(var.fruits)}"
+    #command="echo fruit name -${var.fruits[count.index]}"
+    command="echo fruit name -${var.fruits[each.key]} -- ${var.fruits[each.value]}"
   }
 }
 
