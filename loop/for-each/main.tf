1ccae694f033ce48a7cd809d8c7fe48ef6d198ea
Manikanta <manikanta.guntika@gmail.com>
2023-11-05 00:04:55 +0530

    loop

--- loop/for-each/main.tf
+++ loop/for-each/main.tf
@@ -4,16 +4,36 @@ resource "null_resource" "fruits" {
   provisioner "local-exec" {
     #command="echo fruit name -${length(var.fruits)}"
     #command="echo fruit name -${var.fruits[count.index]}"
-    command="echo fruit name - ${each.key} -- ${each.value}"
+    command="echo fruit name - ${each.key} -- ${each.value["count"]}  -- ${each.value["name"]}"
   }
 }
 
+#variable "fruits" {
+#  default = {
+#    apple=10
+#    orange=20
+#    banana=30
+#
+#  }
+#
+#}
 variable "fruits" {
   default = {
-    apple=10
-    orange=20
-    banana=30
+    apple=
+    {
+      count=10
+      name="first"
+    }
+    orange={
+     count=20
+      name="second"
+    }
+    banana={
+      count=30
+      name="third"
+    }
 
   }
 
 }
+
